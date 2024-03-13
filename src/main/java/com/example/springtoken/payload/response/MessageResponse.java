fac58a1f62e0d65e69489860f452db484d3339fc
Gali Asmuzi <63879587+galiasmu@users.noreply.github.com>
2024-03-13 14:55:06 -0300

    se agregan APIs para probar con todo lo trabajado

---
+++ src/main/java/com/example/springtoken/payload/response/MessageResponse.java
@@ -0,0 +1,17 @@
+package com.example.springtoken.payload.response;
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
